5e6f507ab68505e9f04042a136ccd328f48e8a00
AkmalRegov <m.akmal@regovtech.com>
2023-10-25 16:42:17 +0800

    add async code to simulate API call and user inputs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Engine } = require("json-rules-engine");
+const prompt = require('prompt-sync')();
 const { config: issuanceConfig } = require("./rules-config/issuance.config");
 
 /**
@@ -7,67 +8,86 @@ const { config: issuanceConfig } = require("./rules-config/issuance.config");
 let engine = new Engine();
 
 let facts = {
-    orgDid: "def4567",
-    schemaId: "schemaY",
-    issuanceConfig: issuanceConfig
+  issuanceConfig: issuanceConfig,
 };
+
+// facts.orgDid = prompt("orgDid: ");
+// facts.schemaId = prompt("schemaId: ");
 
 let issuanceRule = {
   conditions: {
     any: [
-    //   {
-    //     fact: "isSchemaAllowedForOrg",
-    //     operator: "equal",
-    //     value: false,
-    //   },
       {
-        fact: "issuanceConfig",
-        operator: "doesNotContain",
-        value: facts.schemaId,
-        path: `$.${facts.orgDid}`
+        fact: "schemaId",
+        operator: "notIn",
+        value: {
+          fact: "allowedSchema"
+        },
       },
-      {
-        fact: "issuanceConfig",
-        operator: "equal",
-        value: undefined,
-        path: `$.${facts.orgDid}`,
-      }
     ],
   },
   event: {
-    type: 'schemaNotAllowedForOrg',
+    type: "schemaNotAllowedForOrg",
     params: {
-        message: 'does not have the permission to use schemaId'
-    }
+      message: "does not have the permission to use schemaId",
+    },
   }
 };
-
-// //Checker function here
-// function checkOrgAllowedSchema(orgDid, schemaId) {
-//   return issuanceConfig[orgDid] != undefined
-//     ? issuanceConfig[orgDid].some((data) => data == schemaId)
-//     : false;
-// }
 
 /**
  * Simulate API call here
  */
-// engine.addFact('isSchemaAllowedForOrg', async function (params, almanac) {
-//     // console.log("almanac is: ", almanac);
-//     const orgDid = await almanac.factValue('orgDid');
-//     const schemaId = await almanac.factValue('schemaId');
-//     return checkOrgAllowedSchema(orgDid, schemaId);
-//     // return almanac.factValue('orgDid')
-//     // .then(orgDid => {
-//     //     return checkOrgAllowedSchema(orgDid, schemaId)
-//     // })
-// })
+engine.addFact('orgDid', async function (params, almanac) {
+    return await getOrgDid();
+})
+
+engine.addFact('schemaId', async function (params, alamnac) {
+  return await getSchemaId();
+})
+
+engine.addFact('allowedSchema', async function (params, almanac) {
+  const orgDid = await almanac.factValue('orgDid');
+  const issuanceConfig = await almanac.factValue('issuanceConfig');
+  return await issuanceConfig[orgDid] != undefined ? issuanceConfig[orgDid] : false;
+})
 
 engine.addRule(issuanceRule);
-
-// Run the engine to evaluate
-engine.run(facts).then(({ events }) => {
-  events.map((event) => console.log(
-    `The issuance was blocked as orgDid ${facts.orgDid} ${event.params.message} ${facts.schemaId}`
-  ));
+engine.on("success", async (event, almanac, ruleResult) => {
+  if (event.type == "schemaNotAllowedForOrg") {
+    const orgDid = await almanac.factValue("orgDid");
+    const schemaId = await almanac.factValue("schemaId");
+    console.log(
+      `The issuance was blocked as orgDid ${orgDid} ${event.params.message} ${schemaId}`,
+    );
+  }
 });
+
+engine.on("failure", async (event, almanac, ruleResult) => {
+  if(event.type == "schemaNotAllowedForOrg") {
+    const orgDid = await almanac.factValue("orgDid");
+    const schemaId = await almanac.factValue("schemaId");
+    console.log(`valid issuance for orgDid ${orgDid} to issue using schemaId ${schemaId}`);
+  }
+})
+
+async function getOrgDid() {
+  return await new Promise((resolve, reject) => {
+    let result = prompt("orgDid: ");
+    // const result = "def456";
+    resolve(result);
+  })
+}
+
+async function getSchemaId() {
+  return await new Promise((resolve, reject) => {
+    let result = prompt("schemaId: ");
+    // const result = "schemaY";
+    resolve(result);
+  })
+}
+
+async function main() {
+  console.log(facts);
+  engine.run(facts);
+}
+main();
